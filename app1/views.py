7c14b7499188042f68f63383ed99b491489e713a
saiklayan <askalyan06@gmail.com>
2022-03-17 18:09:33 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def app1_rcb(request):
+    return HttpResponse('E Sala Cup Namde')
---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def app2_csk(request):
+    return HttpResponse('Whistle Podu')
